422688902682842573e2d8e3cb069e5f666aec1c
nrpradh <wsense911@gmail.com>
2025-04-12 20:27:04 +0700

    Configured CSS

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-6xl font-light font-stretch-ultra-condensed text-blue-200 bg'> BintaroHub</h1>
-        
+        <h1 className='text-6xl font-lsSemibold  font-stretch-ultra-condensed text-allWhite bg-primary p-8'> BintaroHub</h1>
+        {/* <h2 className='welcome testUtil'> Welcome! </h2> */}
       </div>
      
     </>
